670011d511e972ac3f97b0d12a1f63f9d00eb8b4
sizukutamago <sizukutamago@gmail.com>
2024-12-28 23:33:05 +0900

    fix

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -31,116 +31,154 @@ fn main() {
             }
             _ => {
                 // 式の値の計算
-                let left = eval_token(&tokens[0], &memory);
-                let right = eval_token(&tokens[2], &memory);
-                let result = eval_expression(left, &tokens[1], right);
+                let result = eval_expression(&tokens, &memory);
 
                 print_output(result);
                 prev_result = result;
-        }
-
-    }
-}
-
-struct Memory {
-    slots: HashMap<String, f64>,
-}
-
-impl Memory {
-    fn new() -> Self {
-        Self {
-            slots: HashMap::new(),
-        }
-    }
-
-    fn add(&mut self, slot_name: String, prev_result: f64) {
-        match self.slots.entry(slot_name) {
-            Entry::Occupied(mut entry) => {
-                // メモリが見つかったので値を更新
-                *entry.get_mut() += prev_result;
-                *entry.get();
-            }
-            Entry::Vacant(entry) => {
-                // メモリが見つからないので要素追加
-                entry.insert(prev_result);
-                prev_result;
             }
         }
     }
 
-    fn get(&self, slot_name: &str) -> f64 {
-        self.slots.get(slot_name).copied().unwrap_or(0.0)
+    struct Memory {
+        slots: HashMap<String, f64>,
     }
-}
 
-#[derive(Debug, PartialEq)]
-enum Token {
-    Number(f64),
-    MemoryRef(String),
-    MemoryPlus(String),
-    MemoryMinus(String),
-    Plus,
-    Minus,
-    Asterisk,
-    Slash,
-}
+    impl Memory {
+        fn new() -> Self {
+            Self {
+                slots: HashMap::new(),
+            }
+        }
 
-impl Token {
-    fn parse(value: &str) -> Self {
-        match value {
-            "+" => Self::Plus,
-            "-" => Self::Minus,
-            "*" => Self::Asterisk,
-            "/" => Self::Slash,
-            _ if value.starts_with("mem") => {
-                let mut memory_name = value[3..].to_string();
-                if value.ends_with('+') {
-                    memory_name.pop();
-                    Self::MemoryPlus(memory_name)
-                } else if value.ends_with('-') {
-                    memory_name.pop();
-                    Self::MemoryMinus(memory_name)
-                } else {
-                    Self::MemoryRef(memory_name)
+        fn add(&mut self, slot_name: String, prev_result: f64) -> f64 {
+            match self.slots.entry(slot_name) {
+                Entry::Occupied(mut entry) => {
+                    // メモリが見つかったので値を更新
+                    *entry.get_mut() += prev_result;
+                    *entry.get()
+                }
+                Entry::Vacant(entry) => {
+                    // メモリが見つからないので要素追加
+                    entry.insert(prev_result);
+                    prev_result
                 }
             }
-            _ => Self::Number(value.parse().unwrap()),
+        }
+
+        fn get(&self, slot_name: &str) -> f64 {
+            self.slots.get(slot_name).copied().unwrap_or(0.0)
         }
     }
 
-    fn split(text: &str) -> Vec<Self> {
-        text.split(char::is_whitespace).map(Self::parse).collect()
+    #[derive(Debug, PartialEq)]
+    enum Token {
+        Number(f64),
+        MemoryRef(String),
+        MemoryPlus(String),
+        MemoryMinus(String),
+        Plus,
+        Minus,
+        Asterisk,
+        Slash,
     }
-}
 
-fn eval_token(token: &Token, memory: &Memory) -> f64 {
-    match token {
-        Token::Number(value) => {
-            // 数値の場合はそのまま返す
-            *value
+    impl Token {
+        fn parse(value: &str) -> Self {
+            match value {
+                "+" => Self::Plus,
+                "-" => Self::Minus,
+                "*" => Self::Asterisk,
+                "/" => Self::Slash,
+                _ if value.starts_with("mem") => {
+                    let mut memory_name = value[3..].to_string();
+                    if value.ends_with('+') {
+                        memory_name.pop();
+                        Self::MemoryPlus(memory_name)
+                    } else if value.ends_with('-') {
+                        memory_name.pop();
+                        Self::MemoryMinus(memory_name)
+                    } else {
+                        Self::MemoryRef(memory_name)
+                    }
+                }
+                _ => Self::Number(value.parse().unwrap()),
+            }
         }
-        Token::MemeryRef(memory_name) => {
-            // メモリ参照の場合はメモリから値を取得
-            memory.get(memory_name)
-        }
-        _ => {
-            // それ以外の場合はエラー
-            unreachable!()
+
+        fn split(text: &str) -> Vec<Self> {
+            text.split(char::is_whitespace).map(Self::parse).collect()
         }
     }
-}
 
-fn eval_expression(left: f64, operator: &Token, right: f64) -> f64 {
-    match operator {
-        Token::Plus => left + right,
-        Token::Minus => left - right,
-        Token::Asterisk => left * right,
-        Token::Slash => left / right,
-        _ =>
-        // 入力が正しいならここには来ない
-        {
-            unreachable!()
+    fn eval_token(token: &Token, memory: &Memory) -> f64 {
+        match token {
+            Token::Number(value) => {
+                // 数値の場合はそのまま返す
+                *value
+            }
+            Token::MemoryRef(memory_name) => {
+                // メモリ参照の場合はメモリから値を取得
+                memory.get(memory_name)
+            }
+            _ => {
+                // それ以外の場合はエラー
+                unreachable!()
+            }
         }
+    }
+
+    fn eval_expression(tokens: &[Token], memory: &Memory) -> f64 {
+        eval_additive(tokens, memory)
+    }
+
+    fn eval_additive(tokens: &[Token], memory: &Memory) -> f64 {
+        let mut index = 0;
+        let mut result;
+
+        (result, index) = eval_multiplicative_expression(tokens, index, memory);
+
+        while index < tokens.len() {
+            match &tokens[index] {
+                Token::Plus => {
+                    let (value, next) = eval_multiplicative_expression(tokens, index + 1, memory);
+
+                    result += value;
+                    index = next;
+                }
+                Token::Minus => {
+                    let (value, next) = eval_multiplicative_expression(tokens, index + 1, memory);
+                    result -= value;
+                    index = next;
+                }
+                _ => break,
+            }
+        }
+        result
+    }
+
+    fn eval_multiplicative_expression(
+        tokens: &[Token],
+        index: usize,
+        memory: &Memory,
+    ) -> (f64, usize) {
+        let mut index = index;
+        let mut result = eval_token(&tokens[index], memory);
+        index += 1;
+
+        while index < tokens.len() {
+            match &tokens[index] {
+                Token::Asterisk => {
+                    result *= eval_token(&tokens[index + 1], memory);
+                    index += 2;
+                }
+                Token::Slash => {
+                    result /= eval_token(&tokens[index + 1], memory);
+                    index += 2;
+                }
+                _ => break,
+            }
+        }
+        (result, index)
     }
 }
 
