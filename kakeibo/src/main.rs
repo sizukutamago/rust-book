ca6bf9f86561dc40e957bbb0a5f65972de092ecd
sizukutamago <sizukutamago@gmail.com>
2024-12-29 19:12:35 +0900

    add subcommand

---
+++ kakeibo/src/main.rs
@@ -0,0 +1,25 @@
+use clap::{Parser, Subcommand};
+#[derive(Parser)]
+#[clap(version = "1.0")]
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// 新規口座作成
+    New,
+    /// 口座に入金
+    Deposit,
+    /// 口座から出金
+    Withdraw,
+    /// CSVからインポート
+    Import,
+    /// レポート出力
+    Report,
+}
+
+fn main() {
+    let _args = App::parse();
+}
